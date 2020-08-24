3488cb7440f43de13f2fbd8968330826af2cdd48
mohab akram <mohapmobile@gmail.com>
2020-08-24 15:36:12 +0200

    fix lenting errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,16 +1,25 @@
 #!/usr/bin/env ruby
 
 puts 'Game Started!'
+
 puts 'Who is player one?'
+
 user1 = gets.chomp
+
 puts 'Who is player two?'
+
 user2 = gets.chomp
 
-for i in 1..9 do
-puts "It's your move #{user1}! You are O!"
-user1move = gets.chomp
-puts "Now, your move is displayed on the board #{user1}"
-puts "It's your move #{user2}! You are X!"
-user2move = gets.chomp
-puts "Now, your move is displayed on the board #{user2}"
+9.times do
+  puts "It's your move #{user1}! You are O!"
+
+  gets.chomp
+
+  puts "Now, your move is displayed on the board #{user1}"
+
+  puts "It's your move #{user2}! You are X!"
+
+   gets.chomp
+
+  puts "Now, your move is displayed on the board #{user2}"
 end
