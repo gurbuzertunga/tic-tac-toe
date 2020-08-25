91153fa237ae728f3219ffb72eb7db57761a4941
mohab akram <mohapmobile@gmail>
2020-08-25 10:31:49 +0200

    update userinterface

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,14 @@
 #!/usr/bin/env ruby
+# rubocop:disable Metrics/LineLength, Metrics/BlockNesting
+
+played_moves = []
+
+board = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+]
+
 puts 'Game Started!'
 
 puts 'Who is player one?'
@@ -9,16 +19,50 @@ puts 'Who is player two?'
 
 user2 = gets.chomp
 
-9.times do
-  puts "It's your move #{user1}! You are O!"
+game_running = true
 
-  gets.chomp
+counter = 0
 
-  puts "Now, your move is displayed on the board #{user1}"
+current_player = 1
 
-  puts "It's your move #{user2}! You are X!"
-
-  gets.chomp
-
-  puts "Now, your move is displayed on the board #{user2}"
+def display_board(board)
+  board.each do |row|
+    puts
+    row.each do |block|
+      print block.to_s + ' '
+    end
+    puts
+  end
+  puts
 end
+
+while game_running
+
+  puts "It's your move #{current_player == 1 ? user1 : user2}! You are  #{current_player == 1 ? 'o' : 'x'}!"
+
+  current_move = gets.chomp.to_i
+
+  if (1..9).cover?(current_move)
+    if played_moves.include?(current_move)
+      puts 'this is an invalid move'
+      puts 'this block is aready taken'
+    else
+      played_moves.push(current_move)
+      puts "Now, your move is displayed on the board #{current_player == 1 ? user1 : user2}"
+      display_board(board)
+      counter += 1
+      if counter > 2
+        puts 'this is a winning move'
+        game_running = false
+      end
+
+      current_player = current_player == 1 ? 2 : 1
+
+    end
+  else
+    puts 'this is an invalid move'
+    puts 'please enter a number from 1 to 9'
+  end
+
+end
+# rubocop:enable Metrics/LineLength, Metrics/BlockNesting
